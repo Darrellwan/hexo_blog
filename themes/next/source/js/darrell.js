6b3c49b42d7b1c8bb9d9eb62c190e84b46102c65
DarrellWan <darrellwang74@gmail.com>
2022-07-22 01:47:46 +0800

    [FIX][firebase_push] modify get token modify order

--- themes/next/source/js/darrell.js
+++ themes/next/source/js/darrell.js
@@ -167,11 +167,6 @@ function showToken(currentToken) {
     }, 500)
   }
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
   // Your web app's Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyCbDuQldDXIGUYwszFngMPz-dsSKac5shw",
