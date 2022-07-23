359502b70ecf802ea40909b8771f97e7745883d6
DarrellWan <darrellwang74@gmail.com>
2022-07-23 23:29:57 +0800

    [DEV][service_worker] fix service worker

--- source/sw.js
+++ source/sw.js
@@ -7,34 +7,34 @@
 importScripts("https://www.gstatic.com/firebasejs/8.8.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.8.1/firebase-messaging.js");
 
-self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
-    // fcp_options.link field from the FCM backend service goes there, but as the host differ, it not handled by Firebase JS Client sdk, so custom handling
-    console.log({check_true: event.notification && event.notification.data && event.notification.data.FCM_MSG && event.notification.data.FCM_MSG.notification})
-    if (event.notification && event.notification.data && event.notification.data.FCM_MSG && event.notification.data.FCM_MSG.notification) {
-        const url = event.notification.data.FCM_MSG.notification.click_action;
-        console.log({url: url})
+// self.addEventListener('notificationclick', function(event) {
+//     event.notification.close();
+//     // fcp_options.link field from the FCM backend service goes there, but as the host differ, it not handled by Firebase JS Client sdk, so custom handling
+//     console.log({check_true: event.notification && event.notification.data && event.notification.data.FCM_MSG && event.notification.data.FCM_MSG.notification})
+//     if (event.notification && event.notification.data && event.notification.data.FCM_MSG && event.notification.data.FCM_MSG.notification) {
+//         const url = event.notification.data.FCM_MSG.notification.click_action;
+//         console.log({url: url})
 
-        event.waitUntil(
-            self.clients.matchAll({type: 'window'}).then( windowClients => {
-                // Check if there is already a window/tab open with the target URL
-               for (i = 0; i < clients.length; i++) {
-                  if (clients[i].url.indexOf(scopeUrl) !== -1) {
-                        // Scope url is the part of main url
-                        clients[i].navigate(redirectUrl);
-                        return clients[i].focus();
-                        break;
-                  }
-                }
-                // If not, then open the target URL in a new window/tab.
-                if (self.clients.openWindow) {
-                    console.log("open window")
-                    return self.clients.openWindow(url);
-                }
-            })
-        )
-    }
-}, false);
+//         event.waitUntil(
+//             self.clients.matchAll({type: 'window'}).then( windowClients => {
+//                 // Check if there is already a window/tab open with the target URL
+//                for (i = 0; i < clients.length; i++) {
+//                   if (clients[i].url.indexOf(scopeUrl) !== -1) {
+//                         // Scope url is the part of main url
+//                         clients[i].navigate(redirectUrl);
+//                         return clients[i].focus();
+//                         break;
+//                   }
+//                 }
+//                 // If not, then open the target URL in a new window/tab.
+//                 if (self.clients.openWindow) {
+//                     console.log("open window")
+//                     return self.clients.openWindow(url);
+//                 }
+//             })
+//         )
+//     }
+// }, false);
 
 firebase.initializeApp({
     apiKey: "AIzaSyCbDuQldDXIGUYwszFngMPz-dsSKac5shw",
