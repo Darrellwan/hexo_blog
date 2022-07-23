e0eeebd6055ec59589ecabe6c780fc7ff24812cd
DarrellWan <darrellwang74@gmail.com>
2022-07-24 00:26:23 +0800

    [DEV][service_worker] fix service worker

--- source/sw.js
+++ source/sw.js
@@ -9,21 +9,18 @@ importScripts("https://www.gstatic.com/firebasejs/8.8.1/firebase-messaging.js");
 
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
-    // fcp_options.link field from the FCM backend service goes there, but as the host differ, it not handled by Firebase JS Client sdk, so custom handling
-    console.log({check_true: event.notification && event.notification.data && event.notification.data.FCM_MSG && event.notification.data.FCM_MSG.notification})
     if (event.notification && event.notification.data && event.notification.data.FCM_MSG && event.notification.data.FCM_MSG.notification) {
         const url = event.notification.data.FCM_MSG.notification.click_action;
-        console.log({url: url})
 
         event.waitUntil(
             self.clients.matchAll({type: 'window'}).then( windowClients => {
                 // Check if there is already a window/tab open with the target URL
-               for (i = 0; i < clients.length; i++) {
-                console.log({clients: clients[i]})
-                  if (clients[i].url.indexOf(scopeUrl) !== -1 && 'focus' in clients[i]) {
-                        // Scope url is the part of main url
-                        return self.clients[i].focus();
-                  }
+                for (var i = 0; i < windowClients.length; i++) {
+                    var client = windowClients[i];
+                    // If so, just focus it.
+                    if (client.url === url && 'focus' in client) {
+                        return client.focus();
+                    }
                 }
                 // If not, then open the target URL in a new window/tab.
                 if (self.clients.openWindow) {
@@ -44,26 +41,6 @@ firebase.initializeApp({
     appId: "1:712933270955:web:f958378386628d950a9eee"
 });
 const messaging = firebase.messaging();
-
-
-messaging.setBackgroundMessageHandler(function(payload) {
-    console.log('[firebase-messaging-sw.js] setBackgroundMessageHandler message ', payload);
-  // Customize notification here
-  var notificationTitle = payload.data.title; //or payload.notification or whatever your payload is
-  var notificationOptions = {
-    body: payload.data.body,
-    icon: payload.data.icon,
-    data: { url:payload.data.click_action }, //the url which we gonna use later
-    actions: []
-  };
-
-  console.log({notificationTitle: notificationTitle})
-  console.log({notificationOptions: notificationOptions})
-
-  return self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
-
 
 messaging.onBackgroundMessage(function(payload) {
   console.log('[firebase-messaging-sw.js] onBackgroundMessage message ', payload);
