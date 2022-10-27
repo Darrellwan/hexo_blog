87050778eebf8195a7cc042ea0d903b349c1615e
Darrell Wang <darrellwang74@gmail.com>
2022-10-27 15:21:20 +0800

    [UPDATE][FEATURE] add lazyload by lazysize

--- themes/next/scripts/tags/darrell-lightimage.js
+++ themes/next/scripts/tags/darrell-lightimage.js
@@ -9,11 +9,34 @@ function customLightGallery(args, content) {
   if(!altText || !imageSrc){
     return ``
   }
+  // <img
+	// alt="Jellyfish"
+	// sizes="(min-width: 1000px) 930px, 90vw"
+	// data-srcset="small.jpg 500w,
+	// 	medium.jpg 640w,
+	// 	big.jpg 1024w"
+	// data-src="medium.jpg"
+	// class="lazyload" />
   
   return `
   <figure lg-background-color="#282828" class="blog-images" data-src="${imageSrc}">
-    <img alt="${altText}" src="${imageSrc}">
+    <img alt="${altText}" data-src="${imageSrc}" class="lazyload" sizes="(min-width: 1000px) 930px, 90vw">
+  </figure>`
+}
+
+function customLightGalleryCover(args) {
+  let altText = args[0];
+  let imageSrc = args[1];
+
+  if(!altText || !imageSrc){
+    return ``
+  }
+  
+  return `
+  <figure lg-background-color="#282828" class="blog-images" data-src="${imageSrc}">
+    <img alt="${altText}" data-src="${imageSrc}" class="lazyload" sizes="(min-width: 1000px) 930px, 90vw">
   </figure>`
 }
 
 hexo.extend.tag.register('darrellImage', customLightGallery, {ends: false});
+hexo.extend.tag.register('darrellImageCover', customLightGalleryCover, {ends: false});
