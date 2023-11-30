c17700927bcc551d9ed5c0adafb7a6e8dd63486e
Darrell_Kra <darrellwang74@gmail.com>
2023-11-30 11:44:51 +0800

    [Update] structured data update

--- themes/next/scripts/helpers/custom_structure_data.js
+++ themes/next/scripts/helpers/custom_structure_data.js
@@ -34,3 +34,67 @@ function darrellStructuredData() {
 }
 
 hexo.extend.helper.register("darrell_structured_data", darrellStructuredData);
+
+/**
+ * Builds JSON-LD structured data for current page according to its type (page or post).
+ *
+ * @returns {string} - JSON-LD structured data
+ */
+function darrellOrgStructuredData() {
+
+  /**
+   * 
+   * <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "OnlineBusiness",
+      "name": "我的線上書店",
+      "url": "http://www.myonlinebookstore.com",
+      "logo": "http://www.myonlinebookstore.com/logo.png",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "中山北路一段",
+        "addressLocality": "台北市",
+        "postalCode": "104",
+        "addressCountry": "TW"
+      },
+      "contactPoint": {
+        "@type": "ContactPoint",
+        "telephone": "+886-2-1234-5678",
+        "contactType": "客戶服務"
+      },
+      "founder": "張三",
+      "foundingDate": "2010-01-01",
+      "email": "info@myonlinebookstore.com"
+    }
+    </script>
+   */
+
+  schema = {
+    "@context": "https://schema.org",
+    "@type": "OnlineBusiness",
+    "name": "Darrell Martech TW",
+    "url": "https://www.darrelltw.com/",
+    "logo": "https://www.darrelltw.com/images/darrell_icon_512.png",
+    "address": {
+      "@type": "PostalAddress",
+      "addressLocality": "台北市",
+      "addressCountry": "TW"
+    },
+    "contactPoint": {
+      "@type": "ContactPoint",
+      "email": "service@darrelltw.com",
+      "contactType": "技術服務",
+      "areaServed": ["TW", "US", "JP", "TH", "CN", "MY", "SG"],
+      "availableLanguage": ["中文", "English"]
+    },
+    "founder": "Darrell Wang",
+    "email": "service@darrelltw.com"
+  };
+
+  return (
+    '<script type="application/ld+json">' + JSON.stringify(schema) + "</script>"
+  );
+}
+
+hexo.extend.helper.register("darrell_org_structured_data", darrellOrgStructuredData);
