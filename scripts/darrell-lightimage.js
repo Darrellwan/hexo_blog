f44f505ea5205cdcfe54f77e414b23f7ca5746af
Darrell Wang <darrellwang74@gmail.com>
2024-09-24 08:36:38 +0800

    Modify theme to even to even theme

---
+++ scripts/darrell-lightimage.js
@@ -0,0 +1,111 @@
+/* global hexo */
+
+'use strict';
+
+function customLightGallery(args, content) {
+  let altText = args[0];
+  let imageSrc = args[1];
+  let className = args[2];
+
+  if(!className || className == ""){
+    className = "max-1024";
+  }
+
+  if(!altText || !imageSrc){
+    return ``
+  }
+  // <img
+	// alt="Jellyfish"
+	// sizes="(min-width: 1000px) 930px, 90vw"
+	// data-srcset="small.jpg 500w,
+	// 	medium.jpg 640w,
+	// 	big.jpg 1024w"
+	// data-src="medium.jpg"
+	// class="lazyload" />
+  
+  return `
+  <figure lg-background-color="#282828" class="blog-images" data-src="${imageSrc}">
+    <img alt="${altText}" data-src="${imageSrc}" class="lazyload ${className}" sizes="(min-width: 1000px) 930px, 90vw">
+  </figure>`
+}
+
+function customLightGallery800(args, content) {
+  let altText = args[0];
+  let imageSrc = args[1];
+  let className = args[2];
+
+  if(!className || className == ""){
+    className = "max-800";
+  }
+
+  if(!altText || !imageSrc){
+    return ``
+  }
+  
+  return `
+  <figure lg-background-color="#282828" class="blog-images" data-src="${imageSrc}">
+    <img alt="${altText}" data-src="${imageSrc}" class="lazyload ${className}" sizes="(min-width: 800px) 930px, 90vw">
+  </figure>`
+}
+
+
+function customLightGallery800h(args, content) {
+  let altText = args[0];
+  let imageSrc = args[1];
+  let className = args[2];
+
+  if(!className || className == ""){
+    className = "max-800h";
+  }
+
+  if(!altText || !imageSrc){
+    return ``
+  }
+  
+  return `
+  <figure lg-background-color="#282828" class="blog-images" data-src="${imageSrc}">
+    <img alt="${altText}" data-src="${imageSrc}" class="lazyload ${className}" sizes="(max-width: 100%) 930px, 90vw">
+  </figure>`
+}
+
+function customLightGalleryCover(args) {
+  let altText = args[0];
+  let imageSrc = args[1];
+  let className = args[2];
+
+  if(!className || className == ""){
+    className = "max-1024";
+  }
+
+  if(!altText || !imageSrc){
+    return ``
+  }
+  
+  return `
+  <figure lg-background-color="#282828" class="blog-images" data-src="${imageSrc}" style="min-height: 200px;">
+    <img alt="${altText}" data-src="${imageSrc}" src="${imageSrc}" class="${className}" sizes="(min-width: 1000px) 930px, 90vw" style="object-fit: contain;">
+  </figure>`
+}
+
+function customOnlyImage(args) {
+  let altText = args[0];
+  let imageSrc = args[1];
+  let className = args[2];
+
+  if(!className || className == ""){
+    className = "max-1024";
+  }
+
+  if(!altText || !imageSrc){
+    return ``
+  }
+  
+  return `
+  <img alt="${altText}" data-src="${imageSrc}" src="${imageSrc}" class="${className}">`
+}
+
+hexo.extend.tag.register('darrellImage', customLightGallery, {ends: false});
+hexo.extend.tag.register('darrellOnlyImage', customOnlyImage, {ends: false});
+hexo.extend.tag.register('darrellImage800', customLightGallery800, {ends: false});
+hexo.extend.tag.register('darrellImageh800', customLightGallery800h, {ends: false});
+hexo.extend.tag.register('darrellImageCover', customLightGalleryCover, {ends: false});
